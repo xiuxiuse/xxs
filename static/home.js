4653e49f776fb2f548418d0ec497041a44f62a77
xiuxiuse <89770716+xiuxiuse@users.noreply.github.com>
2021-08-30 18:50:59 +0800

    Add files via upload

---
+++ static/home.js
@@ -0,0 +1,23 @@
+(function(w) {
+    function enterLine() {
+        document.getElementById("btnEnter1").onclick = function() {
+            window.open('https://www.xxs.tax', "_self");
+        }
+        document.getElementById("btnEnter2").onclick = function() {
+            window.open('https://www.xxs.icu', "_self");
+        }
+        document.getElementById("btnEnter3").onclick = function() {
+            window.open('https://www.xxs.vin', "_self");
+        }
+        document.getElementById("btnEnter4").onclick = function() {
+            window.open('https://www.xxse.org', "_self");
+        }
+        document.getElementById("btnEnter6").onclick = function() {
+            window.open('http://www.neea.edu.cn/', "_blank");
+        }
+    }
+    function init() {
+        enterLine();
+    }
+    window.addEventListener("DOMContentLoaded", init, !1);
+})(window);
