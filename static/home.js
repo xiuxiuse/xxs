af0a0615207a6d990a0976644a44f76191c93dc3
xiuxiuse <89770716+xiuxiuse@users.noreply.github.com>
2022-03-30 18:20:08 +0800

    Update home.js

--- static/home.js
+++ static/home.js
@@ -4,13 +4,13 @@
             window.open('https://www.xxs.best', "_self");
         }
         document.getElementById("btnEnter2").onclick = function() {
-            window.open('https://www.xxs.icu', "_self");
+            window.open('https://www.xiuxiuse3.com', "_self");
         }
         document.getElementById("btnEnter3").onclick = function() {
-            window.open('https://www.xiuxiuse2.com', "_self");
+            window.open('https://www.xiuxiuse4.com', "_self");
         }
         document.getElementById("btnEnter4").onclick = function() {
-            window.open('https://www.xxse.org', "_self");
+            window.open('https://www.xiuxiuse5.com', "_self");
         }
         document.getElementById("btnEnter6").onclick = function() {
             window.open('http://www.neea.edu.cn/', "_blank");
